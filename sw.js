20a5624a771254d07fd9b3056cd00d3a0eddea0f
RonnyHoebers <ronny.hoebers@gmail.com>
2026-09-13 13:55:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='stalmanager-v3-full';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request))));
+const CACHE="stalmanager-v5-3";
+const ASSETS=["./","./index.html","./manifest.json","./icon.png","./paarden.jpg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>r))));
